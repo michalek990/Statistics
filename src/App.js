4098c84048e3dde5457801f15ef42f713b51b6a7
michalek990 <93988101+michalek990@users.noreply.github.com>
2024-12-27 12:59:59 +0100

    remove charts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Charts from "./components/Charts/Charts";
 import ContactPage from "./components/Contact/Contact";
 
 function App() {
@@ -10,7 +9,7 @@ function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<h1 style={{ textAlign: "center", margin: "20px" }}>Witamy w aplikacji</h1>} />
-                <Route path="/charts" element={<Charts />} />
+                <Route path="/charts"/>
                 <Route path="/contact" element={<ContactPage />} />
             </Routes>
         </Router>
